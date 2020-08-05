8e9ba038325ce0fdf9a09690c7fa264bb3d8caef
Abhay Raj Singh Rathore <sr.abhayraj@gmail.com>
2020-08-05 12:51:19 +0530

    Add files via upload

---
+++ docker_start.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2
+
+import commands as sp
+
+print("content-type: text/html")
+
+cmd = "sudo systemctl restart docker"
+
+output = sp.getstatusoutput(cmd)
+
+if output[0] == 0:
+	print("location: docker.py")
+	print("")
+else:
+	print("")
+	print("not started")
